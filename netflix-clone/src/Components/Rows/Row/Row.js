34ecda96409d7cab5c3014d7c69f09e6610ff976
Yohannes-Developer <yohanneskewani@mu.edu.et>
2025-05-19 15:33:08 +0300

    Row.js is updated to show a trailer of the movie

--- netflix-clone/src/Components/Rows/Row/Row.js
+++ netflix-clone/src/Components/Rows/Row/Row.js
@@ -1,13 +1,15 @@
-// Row.js (Generic Row component)
+// Row.js
 import React, { useEffect, useState } from "react";
 import axios from "../../../Utilities/axios";
+import movieTrailer from "movie-trailer";
+import YouTube from "react-youtube";
 import "./Row.css";
-
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     async function fetchData() {
@@ -17,14 +19,36 @@ function Row({ title, fetchUrl, isLargeRow }) {
     fetchData();
   }, [fetchUrl]);
 
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.title || movie?.name || movie?.original_name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log("Trailer not found", error));
+    }
+  };
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
   return (
     <div className="row">
-      <h2>{title}</h2> {/* ✅ This line renders the title */}
+      <h2>{title}</h2>
       <div className="row__posters">
         {movies.map((movie) => (
           <img
             key={movie.id}
-            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            onClick={() => handleClick(movie)}
+            className={`row__poster ${isLargeRow ? "row__posterLarge" : ""}`}
             src={`${base_url}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
             }`}
@@ -32,6 +56,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
           />
         ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
--- netflix-clone/src/Components/Rows/RowList/RowList.js
+++ netflix-clone/src/Components/Rows/RowList/RowList.js
@@ -12,7 +12,7 @@ const RowList = () => {
       />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRatedMovies} />
-      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      {/* <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} /> */}
       <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
       <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
       <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
