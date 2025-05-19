568eda7669db91d44dbe058152f071bd089e0e05
Yohannes-Developer <yohanneskewani@mu.edu.et>
2025-05-19 17:35:18 +0300

    some editions on the header and change of favicon and tab name

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,15 +1,13 @@
 
-import { HashRouter as Router } from "react-router-dom";
-
 import './App.css';
 import Home from '../src/Pages/Home/Home'
 function App() {
   return (
-    <router>
+   
       <div className="App">
         <Home />
       </div>
-    </router>
+  
   );
 }
 
